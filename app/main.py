ef971ba3bbbb7d862ca9509ee4668f79e79cc249
CryptoQwest <morokhovgleb@gmail.com>
2024-10-10 15:01:41 +0200

    'Solution-fixed'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 from calendar import error
-from idlelib.iomenu import errors
 
 
 def format_linter_error(error: dict) -> dict:
-
-
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -14,17 +11,13 @@ def format_linter_error(error: dict) -> dict:
     }
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-
-
     return {
-        errors: [format_linter_error(error) for error in errors],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
 
 def format_linter_report(linter_report: dict) -> list:
-
-
     return [
         format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
