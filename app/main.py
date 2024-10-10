5b64201b960e3c90c292f6bee838f8a0b083e653
CryptoQwest <morokhovgleb@gmail.com>
2024-10-10 14:56:35 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from idlelib.iomenu import errors
 
 
 def format_linter_error(error: dict) -> dict:
+
+
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -12,6 +14,8 @@ def format_linter_error(error: dict) -> dict:
     }
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
+
+
     return {
         errors: [format_linter_error(error) for error in errors],
         "path": file_path,
@@ -19,6 +23,8 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     }
 
 def format_linter_report(linter_report: dict) -> list:
+
+
     return [
         format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
